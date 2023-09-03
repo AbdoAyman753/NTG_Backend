41f55088250daf9af3028a75101f1edadde13367
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-09-03 21:34:56 +0300

    Defined Registration and login routing controller

---
+++ src/main/java/com/ntg/videogames/controllers/AuthenticationController.java
@@ -0,0 +1,33 @@
+package com.ntg.videogames.controllers;
+
+import com.ntg.videogames.entities.User;
+import com.ntg.videogames.services.AuthenticationService;
+import com.ntg.videogames.utils.AuthenticationRequest;
+import com.ntg.videogames.utils.AuthenticationResponse;
+import com.ntg.videogames.utils.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}
