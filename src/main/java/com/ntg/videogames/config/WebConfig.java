3f9e5c2afc15541074d9355236849573f5dc2058
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-09-03 21:33:44 +0300

    Configured Cors Setting for allowing request from frontend

---
+++ src/main/java/com/ntg/videogames/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.ntg.videogames.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Update with the actual origin of your frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify the allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true);
+    }
+}
