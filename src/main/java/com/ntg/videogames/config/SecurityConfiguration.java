d0c7774e2d617f18d6ef52f876519062f5a73be5
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-09-05 21:55:41 +0300

    Allowed Swagger to bypass security measurements

--- src/main/java/com/ntg/videogames/config/SecurityConfiguration.java
+++ src/main/java/com/ntg/videogames/config/SecurityConfiguration.java
@@ -32,6 +32,18 @@ public class SecurityConfiguration {
                 .permitAll()
                 .antMatchers("/api/v1/categories/**")
                 .permitAll()
+                .antMatchers("/api/v1/auth/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/swagger-ui.html")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
